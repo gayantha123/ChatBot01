29ea9aead7b6c9867ba828f9ff36d44cf6dafaeb
GayanthaLochana0 <lochanagayantha70@gmail.com>
2025-01-16 15:15:47 +0530

    .js

--- script.js
+++
@@ -1,8 +0,0 @@
-function updateTime() {
-    const timeElement = document.getElementById('time');
-    const now = new Date();
-    timeElement.innerText = now.toLocaleTimeString();
-}
-
-setInterval(updateTime, 1000);
-updateTime();
